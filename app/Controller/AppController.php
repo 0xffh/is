567ca8f85836d0ef4b927359ee96321c15de503a
Vlad Shylyar <vlad.shylyar@gmail.com>
2015-05-27 20:23:20 +0300

    гыуы blowfish

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -1,5 +1,6 @@
 <?php
 App::uses('Controller', 'Controller');
+App::uses('BlowfishPasswordHasher', 'Controller/Component/Auth');
 class AppController extends Controller {
     public $components = array(
         'Auth' => array(
